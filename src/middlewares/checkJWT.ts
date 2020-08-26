e095cb1268f9b87a62bfffd4b61c31da3be35a3c
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2020-08-26 15:03:27 -0600

    :art: Add auth navigation guard

---
+++ src/middlewares/checkJWT.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import * as jwt from 'jsonwebtoken';
+import config from './../../config/config';
+
+export const checkJWT = (req: Request, res: Response, next: NextFunction) => {
+  //Get the jwt token authorization from headers
+  const token = <string>req.headers['Authorization'];
+  let jwtPayload;
+
+  try {
+    jwtPayload = <any>jwt.verify(token, config.jwtSecret);
+    res.locals.jwtPayload = jwtPayload;
+  } catch (error) {
+    res.sendStatus(401);
+    return;
+  }
+
+  const { userId, email } = jwtPayload;
+  const newToken = jwt.sign({ userId, email }, config.jwtSecret, {
+    expiresIn: '1h',
+  });
+  res.setHeader('token', newToken);
+
+  next();
+};
