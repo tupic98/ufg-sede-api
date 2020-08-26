4b3946f199f94fc0e591382aafd2ea93b80ecfd5
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2020-08-26 14:59:11 -0600

    :tada: Initialize project

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
+import { connectDB } from './database';
+import indexRouter from './routes/index';
+import apiRouter from './routes/api';
+
+const port: Number = Number(process.env.PORT) || 3000;
+const startServer = async () => {
+  const app = express();
+  app.use(cors());
+  app.use(helmet());
+  app.use(bodyParser.json());
+  app.use(express.json());
+
+  app.use('/', indexRouter);
+  app.use('/api', apiRouter);
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+};
+
+(async () => {
+  await connectDB();
+  await startServer();
+})();
