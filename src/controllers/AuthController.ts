1202802a929351b0fb9d6aed2a1d804957d3af0e
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2020-08-26 15:02:22 -0600

    :sparkles: Create basic Auth Controller

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,85 @@
+import { Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+import { getRepository } from 'typeorm';
+import { validate } from 'class-validator';
+
+import { User } from './../entities/User';
+import config from './../../config/config';
+
+class AuthController {
+  static login = async (req: Request, res: Response) => {
+    // Check if username and password are set
+    let { email, password } = req.body;
+    if (!(email && password)) {
+      res.sendStatus(400);
+    }
+
+    // Get user form database
+    const userRepository = getRepository(User);
+    let user: User;
+
+    try {
+      user = await userRepository.findOneOrFail({ where: { email } });
+    } catch (error) {
+      res.sendStatus(401);
+      return;
+    }
+
+    // Check if encrypted password match
+    if (!user.checkIfUnencryptedPasswordIsValid(password)) {
+      res.sendStatus(401);
+      return;
+    }
+
+    //Sign JWT, valid for 1 hour
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      config.jwtSecret,
+      { expiresIn: '1h' }
+    );
+
+    //Send the jwt in the response
+    res.send(token);
+  };
+
+  static changePassword = async (req: Request, res: Response) => {
+    //Get ID from JWT
+    const id = res.locals.jwtPayload.userId;
+
+    //Get parameters from the body
+    const { oldPassword, newPassword } = req.body;
+    if (!(oldPassword && newPassword)) {
+      res.sendStatus(400);
+      return;
+    }
+
+    const userRepository = getRepository(User);
+    let user: User;
+    try {
+      user = await userRepository.findOneOrFail(id);
+    } catch (error) {
+      res.sendStatus(401);
+      return;
+    }
+
+    // Check if old password matchs
+    if (!user.checkIfUnencryptedPasswordIsValid(oldPassword)) {
+      res.sendStatus(401);
+      return;
+    }
+
+    //Validate model (password length)
+    user.password = newPassword;
+    const errors = await validate(user);
+    if (errors.length > 0) {
+      res.status(400).send(errors);
+    }
+
+    user.hashPassword();
+    userRepository.save(user);
+
+    res.sendStatus(204);
+  };
+}
+
+export default AuthController;
