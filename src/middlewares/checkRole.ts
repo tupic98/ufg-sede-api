e2f0a3300beb6cae3633baa027b15dede7b46bc2
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2020-10-05 23:38:30 -0600

    :art: Check role from jwt

--- src/middlewares/checkRole.ts
+++ src/middlewares/checkRole.ts
@@ -1,23 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
-import { getRepository } from 'typeorm';
-
-import { User } from './../entities/User';
 
 export const checkRole = (roles: Array<string>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const id = res.locals.jwtPayload.userId;
+    const role = res.locals.jwtPayload.role;
 
-    const userRepository = getRepository(User);
-    let user: User;
+    // const userRepository = getRepository(User);
+    // let user: User;
 
-    try {
-      user = await userRepository.findOneOrFail(id, { relations: ['role'] });
-    } catch (error) {
-      res.sendStatus(401);
-      return;
-    }
+    // try {
+    //   user = await userRepository.findOneOrFail(id, { relations: ['role'] });
+    // } catch (error) {
+    //   res.sendStatus(401);
+    //   return;
+    // }
 
-    if (roles.indexOf(user.role.name) > -1) next();
+    if (roles.indexOf(role) > -1) next();
     else res.status(401).send();
   };
 };
