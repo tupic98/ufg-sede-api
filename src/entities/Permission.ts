abb63799a5e759f95cfbccd4c4851d6f72455bec
tupic98 <00017316@uca.edu.sv>
2020-10-05 20:52:32 -0600

    :sparkles: Added permission entity

---
+++ src/entities/Permission.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Permission {
+  @PrimaryGeneratedColumn({ name: 'permission_id', type: 'int' })
+  id: number;
+
+  @Column({ name: 'role_name', type: 'varchar', length: '20' })
+  name: string;
+}
