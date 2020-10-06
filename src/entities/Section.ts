59d790a7271c4866a57edb06216972eaa9caaf7c
tupic98 <00017316@uca.edu.sv>
2020-10-05 20:50:51 -0600

    :sparkles: Added section entity

---
+++ src/entities/Section.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Student } from './Student';
+
+@Entity()
+export class Section {
+  @PrimaryGeneratedColumn({ name: 'section_id', type: 'int' })
+  id: number;
+
+  @Column({ name: 'section_name', type: 'varchar', length: '20' })
+  name: string;
+
+  @OneToMany(
+    (type) => Student,
+    (student) => student.section
+  )
+  students: Student[];
+}
