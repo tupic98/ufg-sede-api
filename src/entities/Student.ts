a0e7bbd18b94c9841060cfc2d7bb8d1e88e8a24d
tupic98 <00017316@uca.edu.sv>
2020-10-05 20:50:33 -0600

    :sparkles: Added student entity

---
+++ src/entities/Student.ts
@@ -0,0 +1,79 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Grade } from './Grade';
+import { Modality } from './Modality';
+import { Person } from './Person';
+import { Section } from './Section';
+import { Subject } from './Subject';
+
+@Entity()
+export class Student {
+  @PrimaryGeneratedColumn({ name: 'student_id', type: 'int' })
+  id: number;
+
+  @Column({ name: 'student_year', type: 'smallint' })
+  year: number;
+
+  @Column({ name: 'student_report', type: 'text' })
+  report: string;
+
+  @Column({ name: 'student_approved', type: 'boolean' })
+  approved: boolean;
+
+  @Column({
+    name: 'student_finalAverage',
+    type: 'numeric',
+    precision: 4,
+    scale: 2,
+  })
+  finalAverage: number;
+
+  @Column({
+    name: 'student_institutionalAverage',
+    type: 'numeric',
+    precision: 4,
+    scale: 2,
+  })
+  institutionalAverage: number;
+
+  @Column({ name: 'student_code', type: 'varchar', length: '15' })
+  code: string;
+
+  @Column({ name: 'student_firstTiem', type: 'boolean', default: 'true' })
+  firstTime: boolean;
+
+  @OneToOne((type) => Person)
+  @JoinColumn()
+  person: Person;
+
+  @ManyToOne(
+    (type) => Modality,
+    (modality) => modality.students
+  )
+  modality: Modality;
+
+  @ManyToOne(
+    (type) => Section,
+    (section) => section.students
+  )
+  section: Section;
+
+  @ManyToOne(
+    (type) => Grade,
+    (grade) => grade.students
+  )
+  grade: Grade;
+
+  @OneToMany(
+    (type) => Subject,
+    (subject) => subject.student
+  )
+  subjects: Subject[];
+}
