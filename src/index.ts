224dc4009ee295760e09f273ebe6e5aab9815534
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2020-10-25 15:32:29 -0600

    :fire: Add pagination and container middleware

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as bodyParser from 'body-parser';
 import * as helmet from 'helmet';
 import * as cors from 'cors';
 import { connectDB } from './database';
+import { pagination } from 'typeorm-pagination';
 import indexRouter from './routes/index';
 import apiRouter from './routes/api';
 
@@ -15,6 +16,7 @@ const startServer = async () => {
   app.use(helmet());
   app.use(bodyParser.json());
   app.use(express.json());
+  app.use(pagination);
 
   app.use('/', indexRouter);
   app.use('/api', apiRouter);
