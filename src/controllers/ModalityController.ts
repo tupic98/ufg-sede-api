0ea59bebb5c0ef41ce6902db6172881fc9f44281
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2020-10-25 23:01:50 -0600

    :fire: Add modality controller crud

---
+++ src/controllers/ModalityController.ts
@@ -0,0 +1,89 @@
+import { Request, Response } from 'express';
+import { ModalityService } from '../services/ModalityService';
+import { Container } from 'typedi';
+import { Modality } from '../entities/Modality';
+import { validate } from 'class-validator';
+
+class ModalityController {
+  static fetch = async (req: Request, res: Response) => {
+    const modalityService = Container.get(ModalityService);
+    const modalities = await modalityService.findAll();
+    res.status(200).send(modalities);
+  }
+
+  static store = async (req: Request, res: Response) => {
+    const modalityService = Container.get(ModalityService);
+    const { type }: { type: string } = req.body;
+
+    const modality = new Modality();
+    modality.type = type;
+
+    const modalityErrors = await validate(modality);
+    if (modalityErrors.length > 0) {
+      res.status(400).send(modalityErrors);
+      return;
+    }
+
+    try {
+      await modalityService.create(modality);
+    } catch (e) {
+      res.status(400).json({ message: 'No se pudo crear la modalidad '});
+      return;
+    }
+    res.status(201).send('Modalidad creada correctamente');
+  }
+
+  static update = async (req: Request, res: Response) => {
+    const modalityService = Container.get(ModalityService);
+    const id: number = Number(req.params.id);
+    const { type }: { type: string } = req.body;
+
+    const modality = await modalityService.findById(id);
+    if (!modality) {
+      res.status(404).json({ message: 'Modalidad no encontrada '});
+      return;
+    }
+
+    modality.type = type;
+
+    const modalityErrors = await validate(modality);
+    if (modalityErrors.length > 0) {
+      res.status(400).send(modalityErrors);
+      return;
+    }
+
+    try {
+      await modalityService.update(modality);
+    } catch (e) {
+      res.status(400).json({ message: 'No se pudo actualizar la modalidad '});
+      return;
+    }
+
+    res.status(200).send('Modalidad actualizada');
+  }
+
+  static show = async (req: Request, res: Response) => {
+    const modalityService = Container.get(ModalityService);
+    const id: number = Number(req.params.id);
+    const modality = await modalityService.findById(id);
+    if (!modality) {
+      res.status(404).json({ message: 'Modalidad no encontrada '});
+      return;
+    }
+    res.status(200).send(modality);
+  }
+
+  static destory = async (req: Request, res: Response) => {
+    const modalityService = Container.get(ModalityService);
+    const id: number = Number(req.params.id);
+    const modality = await modalityService.findById(id);
+    if (!modality) {
+      res.status(404).json({ message: 'Modalidad no encontrada '});
+      return;
+    }
+    await modalityService.delete(id);
+    res.status(204).send();
+  }
+}
+
+export default ModalityController;
