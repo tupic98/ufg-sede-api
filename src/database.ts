4ce1e718c9aa8eb96e3b3897b6fe953622b4a156
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2020-10-26 18:47:50 -0600

    Setup to deploy to heroku

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,24 @@
-import { createConnection, useContainer } from 'typeorm';
+import { createConnection, useContainer, ConnectionOptions, getConnectionOptions } from 'typeorm';
 import { Container } from "typedi";
+
+const getOptions = async () => {
+  let connectionOptions: ConnectionOptions;
+  connectionOptions = {
+    type: 'postgres',
+    synchronize: false,
+    logging: true,
+    entities: ['src/entities/**/*.ts', 'dist/entities/**/*.js'],
+  };
+  if (process.env.DATABASE_URL) {
+    Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+  } else {
+    connectionOptions = await getConnectionOptions();
+  }
+  return connectionOptions;
+}
 
 export const connectDB = async () => {
   useContainer(Container);
-  return await createConnection();
+  const typeormConfig = await getOptions();
+  return await createConnection(typeormConfig);
 };
