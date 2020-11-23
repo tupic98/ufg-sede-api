f5b54644e9bde14e2b9b1950230072bfa39f6c57
tupic98 <00017316@uca.edu.sv>
2020-11-22 21:55:34 -0600

    :fire: Custom suject to student many to many relation

---
+++ src/entities/SubjectToStudent.ts
@@ -0,0 +1,26 @@
+import { Student } from './Student';
+import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { Subject } from './Subject';
+import { IsNotEmpty, IsNotEmptyObject } from 'class-validator';
+
+@Entity()
+export class SubjectToStudent {
+    @PrimaryColumn({ name: 'student_code', type: 'varchar', length: '15' })
+    student_code: string
+
+    @ManyToOne(
+        (type) => Student,
+        (student) => student.subjectQualifications
+    )
+    @JoinColumn({ name: 'student_id' })
+    @IsNotEmptyObject()
+    student: Student;
+    
+    @ManyToOne(
+        (type) => Subject,
+        (subject) => subject.studentQualifications
+    )
+    @JoinColumn({ name: 'subject_id ' })
+    @IsNotEmptyObject()
+    subject: Subject;
+}
