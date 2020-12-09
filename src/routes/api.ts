92e4d0579a68ab1b418e054d78596d4edb6a91c0
Rodrigo Alvarenga <ralvarenga@Rodrigos-MacBook-Pro.local>
2020-12-08 19:17:32 -0600

    :art: Updated routes for app

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,14 +1,20 @@
-import { checkStudentJWT } from './../middlewares/checkStudentJWT';
-import { checkStudentCode } from './../middlewares/checkStudentCode';
+import { checkStudentJWT } from '../middlewares/checkStudentJWT';
+import { checkStudentCode } from '../middlewares/checkStudentCode';
 import { Router } from 'express';
 import AdminRouter from './api/admin';
 import AuthRouter from './api/AuthRouter';
 import StudentController from './../controllers/StudentController';
+import SedeController from "../controllers/SedeController";
+import ModuleController from "../controllers/ModuleController";
 
 const router: Router = Router();
 
 router.use('/admin', AdminRouter);
 router.use('/auth', AuthRouter);
-router.get('/notes', [checkStudentJWT, checkStudentCode], StudentController.showByCode);
+router.get('/student/notes', [checkStudentJWT, checkStudentCode], StudentController.showByCode);
+router.get('/student/me', [checkStudentJWT, checkStudentCode], StudentController.me);
+router.post('/student/contact', [checkStudentJWT, checkStudentCode], StudentController.updateContact);
+router.get('/sede-information', SedeController.show);
+router.get('/modules', ModuleController.list);
 
 export default router;
